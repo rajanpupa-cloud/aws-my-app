3ddd045f54092e176d86d9bf5672e27550ab9fa3
Rajan <rajanpupa@gmail.com>
2021-12-20 14:16:53 -0600

    working commit

--- lib/my_ecs_construct-stack.ts
+++ lib/my_ecs_construct-stack.ts
@@ -1,15 +1,29 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as ecs from "@aws-cdk/aws-ecs";
+import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
 // import * as sqs from '@aws-cdk/aws-sqs';
 
 export class MyEcsConstructStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 2 // Default is all AZs in region
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyEcsConstructQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cluster = new ecs.Cluster(this, "MyCluster", {
+      vpc: vpc
+    });
+
+    // Create a load-balanced Fargate service and make it public
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster, // Required
+      cpu: 512, // Default is 256
+      desiredCount: 2, // Default is 1
+      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      memoryLimitMiB: 1024, // Default is 512
+      publicLoadBalancer: true, // Default is false
+    });
   }
 }
